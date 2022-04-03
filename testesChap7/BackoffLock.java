141a74b947481718a4823ba0582fa029c990ec66
Larissa Guder <lariguder10@gmail.com>
2022-04-03 00:05:24 -0300

    feat: backoff

---
+++ testesChap7/BackoffLock.java
@@ -0,0 +1,62 @@
+package testesChap7;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class BackoffLock implements Lock {
+    private AtomicBoolean state = new AtomicBoolean(false);
+    private static final int MIN_DELAY = 2;
+    private static final int MAX_DELAY = 6;
+
+    public void lock() {
+        Backoff backoff = new Backoff(MIN_DELAY, MAX_DELAY);
+        while (true) {
+            while (state.get()) {
+            }
+            ;
+            if (!state.getAndSet(true)) {
+
+                return;
+
+            } else {
+                try {
+                    backoff.backoff();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+    }
+
+    public void unlock() {
+        state.set(false);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Condition newCondition() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
