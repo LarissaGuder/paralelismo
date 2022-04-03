5f67b252865d0e3b8d1b737c97e9277b0adff939
Larissa Guder <lariguder10@gmail.com>
2022-04-03 00:29:12 -0300

    feat: geração de gráficos ao executar

--- testesChap7/Main.java
+++ testesChap7/Main.java
@@ -1,18 +1,23 @@
 package testesChap7;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.locks.*;
 
 class Main {
     static Lock lock;
     static double[] sharedData;
-    static int CS = 1000000; 
-    // TH = 14;
-    // CS: critical section executed per thread
-    // TH: number of threads
+    static int L = 1000000, N = 14, D = 0;
+    static long TASTime1, TASTime2, result;
+
+    // N: 2, 4, 6, 8, 10, 12, 14, 16.  (depende de onde roda)
+    // L: nro de iterações - 1000.000.  (chute)
+    // D:  por enquanto 0. ( nao ha tempo associado ao uso do recurso - unlock é imediato)
 
     static Thread thread(int n) {
         Thread t = new Thread(() -> {
-            for (int i = 0; i < CS; i++) {
+            for (int i = 0; i < L; i++) {
                 lock.lock();
                 lock.unlock();
             }
@@ -21,10 +26,7 @@ class Main {
         return t;
     }
 
-    // Tests to see if threads execute critical
-    // section atomically.
     static void testThreads(int TH) {
-        log("Starting " + TH + " threads ...");
         Thread[] threads = new Thread[TH];
         for (int i = 0; i < TH; i++)
             threads[i] = thread(i);
@@ -36,15 +38,35 @@ class Main {
     }
 
     public static void main(String[] args) {
-        long time1 = System.nanoTime();
-        lock = new TASLock();
-        testThreads(14);
-        long time12 = System.nanoTime();
+        try {
+            PrintWriter writer = new PrintWriter("testesChap7\\graphs\\results.csv", "UTF-8");
+            for (int i = 2; i < N; i = i + 2) {
+                TASTime1 = System.nanoTime();
+                lock = new TASLock();
+                testThreads(i);
+                TASTime2 = System.nanoTime();
+                result = TASTime2 - TASTime1;
 
-        System.out.println("time " + (time12 - time1));
+                TASTime1 = System.nanoTime();
+                lock = new TTASLock();
+                testThreads(i);
+                TASTime2 = System.nanoTime();
+                long resultTTAS = TASTime2 - TASTime1;
+
+                TASTime1 = System.nanoTime();
+                lock = new BackoffLock();
+                testThreads(i);
+                TASTime2 = System.nanoTime();
+                long resultBackOff = TASTime2 - TASTime1;
+
+                writer.println(i + "," + result + "," + resultTTAS + "," + resultBackOff);
+            }
+            writer.close();
+
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    static void log(String x) {
-        System.out.println(x);
-    }
 }
---
+++ testesChap7/graphs/graphs.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import csv
+  
+x = []
+TAS = []
+TTAS = []
+backoff = []
+  
+with open('results.csv','r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    for row in lines:
+        x.append(row[0])
+        TAS.append(int(row[1]))
+        TTAS.append(int(row[2]))
+        backoff.append(int(row[3]))
+        
+plt.plot(x, TAS, color = 'g', linestyle = 'dashed',
+         marker = 'o',label = "TAS")
+plt.plot(x, TTAS, color = 'red', linestyle = 'dashed',
+         marker = 'o',label = "TTAS")
+plt.plot(x, backoff, color = 'blue', linestyle = 'dashed',
+         marker = 'o',label = "backoff")
+
+  
+plt.xticks(rotation = 25)
+plt.xlabel('Threads')
+plt.ylabel('Time')
+plt.title('SpinLock', fontsize = 20)
+plt.grid()
+plt.legend()
+plt.show()
