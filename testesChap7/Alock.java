a3c6f9b5855d7c9e6b4fddfc3c84d0033647b848
Larissa Guder <lariguder10@gmail.com>
2022-04-22 18:58:22 -0300

    feat: Alock

---
+++ testesChap7/Alock.java
@@ -0,0 +1,43 @@
+package testesChap7;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ALock implements Lock {
+    private ThreadLocal<Integer> mySlotIndex = ThreadLocal.withInitial(() -> 0);
+
+    private AtomicInteger tail;
+    private boolean[] flag;
+    private int size;
+
+    /**
+     * Constructor
+     * 
+     * @param capacity max number of array slots
+     */
+    public ALock(int capacity) {
+        size = capacity;
+        tail = new AtomicInteger(0);
+        flag = new boolean[capacity];
+        flag[0] = true;
+    }
+
+    @Override
+    public boolean trylock() {
+        return false;
+    }
+
+    @Override
+    public void lock() {
+        int slot = tail.getAndIncrement() % size;
+        mySlotIndex.set(slot);
+        while (!flag[mySlotIndex.get()]) {
+        }
+        ;
+    }
+
+    @Override
+    public void unlock() {
+        flag[mySlotIndex.get()] = false;
+        flag[(mySlotIndex.get() + 1) % size] = true;
+    }
+}
