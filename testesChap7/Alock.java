1ae2b2ff269d38f3f16e6ffb8572978af85401be
Larissa Guder <lariguder10@gmail.com>
2022-04-27 22:04:11 -0300

    fix: add volatile na flag do alock

--- testesChap7/Alock.java
+++ testesChap7/Alock.java
@@ -13,7 +13,7 @@ public class ALock implements Lock {
     };
 
     AtomicInteger tail;
-    boolean[] flag;
+    volatile boolean[] flag;
     int size;
 
     public ALock(int capacity) {
@@ -39,21 +39,26 @@ public class ALock implements Lock {
 
     @Override
     public void lockInterruptibly() throws InterruptedException {
+        // TODO Auto-generated method stub
 
     }
 
     @Override
     public boolean tryLock() {
+        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public Condition newCondition() {
+        // TODO Auto-generated method stub
         return null;
     }
+
 }
